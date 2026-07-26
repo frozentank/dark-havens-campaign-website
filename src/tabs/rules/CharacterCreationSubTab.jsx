4f412ec835a07232a9c307ea7b85b3d1b7c850a2
Thomas M <frozentank@gmail.com>
2026-07-26 00:34:34 -0400

    feat: adding challenge mode information
    Signed-off-by: Thomas M <frozentank@gmail.com>

--- src/tabs/rules/CharacterCreationSubTab.jsx
+++ src/tabs/rules/CharacterCreationSubTab.jsx
@@ -86,6 +86,42 @@ export default function CharacterCreationSubTab() {
             <li>You may wish to avoid Feats that give bonuses to initiative as we will not be rolling that.</li>
           </ul>
         </div>
+
+        <div>
+          <h3 className="text-xl font-bold text-blue-300 mb-3">Challenge Mode</h3>
+          For those that want to push the boundaries of what a character can be, I would like to encourage you to attempt a character outside the norms of their class's norms. For example:
+          <ul className="list-disc list-inside space-y-2 text-slate-300 mt-2">
+            <li>A wizard that has 8 intelligence</li>
+            <li>A ranged Paladin</li>
+            <li>A Bard with stage fright</li>
+          </ul>
+          <div className="mt-4">
+            To complement this, set the primary state of your class to your lowest score. Here are the primary stats for each class:
+            <ul className="grid grid-cols-1 md:grid-cols-3 gap-x-6 gap-y-2 list-disc list-inside text-slate-300 mt-2">
+              <li>Paladin - Cha/Str</li>
+              <li>Barbarian - Str</li>
+              <li>Bard - Cha</li>
+              <li>Druid - Wis</li>
+              <li>Ranger - Dex/Wis</li>
+              <li>Rogue - Dex</li>
+              <li>Wizard - Int</li>
+              <li>Cleric - Wis</li>
+              <li>Sorcerer - Cha</li>
+              <li>Monk - Str/Dex</li>
+              <li>Fighter - Str</li>
+              <li>Warlock - Cha</li>
+              <li>Artificer - Int</li>
+            </ul>
+          </div>
+          <p className="text-slate-300 mt-2">
+            If you multi-class, you are welcome to shift your scores so that you ensure your classes stay at low scores.
+          </p>
+          <div>
+            <h2 className="text-xl text-slate-100 mt-2 italic">
+              Remember the goal of this is to have fun. It's not to win.
+            </h2>
+          </div>
+        </div>
       </div>
     </SectionCard>
   );
